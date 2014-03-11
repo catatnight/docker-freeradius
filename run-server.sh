fd0b5a03400b8eb1edd3bcc42b234e0c930ea57e
Minghou Ye <minghou.ye@gmail.com>
2014-03-11 11:50:02 +0800

    "docker run -name" is depreciated

--- run-server.sh
+++ run-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 1812:1812/udp -p 1813:1813/udp -name freeradius -d catatnight/freeradius-mysql
+docker run -p 1812:1812/udp -p 1813:1813/udp --name freeradius -d catatnight/freeradius-mysql
