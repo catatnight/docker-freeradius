d3ecb5e05e261c2cecd446574bd794be9bda2a81
Minghou Ye <minghou.ye@gmail.com>
2014-06-11 20:01:57 +0800

    define _start() and _stop()

--- manage.py
+++ manage.py
@@ -7,18 +7,21 @@ if __name__=="__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("execute", help="start|stop|restart freeradius server")
   args = parser.parse_args()
+
+  def _start():
+    bashCommand = "docker run --net=host --name freeradius -d catatnight/freeradius-mysql"
+    process = subprocess.Popen(shlex.split(bashCommand))
+    output = process.communicate()[0]
+
+  def _stop():
+    bashCommand = "docker rm -f freeradius"
+    process = subprocess.Popen(shlex.split(bashCommand))
+    output = process.communicate()[0]
+
   if args.execute== "start":
-    bashCommand = "docker run --net=host --name freeradius -d catatnight/freeradius-mysql"
-    process = subprocess.Popen(shlex.split(bashCommand))
-    output = process.communicate()[0]
+    _start()
   elif args.execute== "stop":
-    bashCommand = "docker rm -f freeradius"
-    process = subprocess.Popen(shlex.split(bashCommand))
-    output = process.communicate()[0]
+    _stop()
   elif args.execute== "restart":
-    bashCommand = "docker rm -f freeradius"
-    process = subprocess.Popen(shlex.split(bashCommand))
-    output = process.communicate()[0]
-    bashCommand = "docker run --net=host --name freeradius -d catatnight/freeradius-mysql"
-    process = subprocess.Popen(shlex.split(bashCommand))
-    output = process.communicate()[0]
+    _stop()
+    _start()
