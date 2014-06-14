5d61546a414d0c5f768c2c4efbee96f6d9408ff3
Minghou Ye <minghou.ye@gmail.com>
2014-06-14 11:53:12 +0800

    format fix

--- assets/install.sh
+++ assets/install.sh
@@ -8,9 +8,9 @@ sed -i -e "/client localhost/i client 0.0.0.0/0{\n\tsecret = $radpass\n}" \
 sed -i -e "s/driver =.*/driver = \"rlm_sql_$sql_driver\"/" \
   -e "s/dialect =.*/dialect = \"$sql_driver\"/" /etc/freeradius/mods-available/sql
 if [[ "$sql_driver" == "mysql" ]]; then
-sed -i "/driver =.*/ a\ \n\tserver = \"$mysql_server\"\n\tlogin = \"$mysql_login\"\n\tpassword = \"$mysql_passwd\"" /etc/freeradius/mods-available/sql
+  sed -i "/driver =.*/ a\ \n\tserver = \"$mysql_server\"\n\tlogin = \"$mysql_login\"\n\tpassword = \"$mysql_passwd\"" /etc/freeradius/mods-available/sql
 elif [[ "$sql_driver" == "sqlite" ]]; then
-sed -i "/driver =.*/ a\ \n\tsqlite {\n\t\tfilename = \"/opt/db/$sqlite_db\"\n\t}" /etc/freeradius/mods-available/sql
+  sed -i "/driver =.*/ a\ \n\tsqlite {\n\t\tfilename = \"/opt/db/$sqlite_db\"\n\t}" /etc/freeradius/mods-available/sql
 fi
 ln -s /etc/freeradius/mods-available/sql /etc/freeradius/mods-enabled/sql
 sed -i '0,/md5/{s/md5/mschapv2/}' /etc/freeradius/mods-available/eap
