6434568074400035fbddc8c282e2c530ef511fec
Minghou Ye <minghou.ye@gmail.com>
2014-09-09 21:15:33 +0800

    add debug mode in manage.py

--- v2/manage.py
+++ v2/manage.py
@@ -15,7 +15,7 @@ class bcolors:
 app_name = 'freeradius'
 
 parser = argparse.ArgumentParser(description='Manage %s container' % app_name)
-parser.add_argument("execute", choices=['create','start','stop','restart','delete'], help='manage %s server' % app_name)
+parser.add_argument("execute", choices=['create','debug','start','stop','restart','delete'], help='manage %s server' % app_name)
 parser.add_argument("-s", "--radius_secret", help="radius server secret ")
 parser.add_argument("--mysql_server", help="mysql server address ")
 parser.add_argument("-u", "--mysql_login", help="mysql server login username ")
@@ -28,11 +28,19 @@ cmd_dict = { \
 		"docker run --net=host -e radpass={1} -e mysql_server={2} -e mysql_login={3} -e mysql_passwd={4} " \
 		"-e readsqlclients={5} -e time_zone=Asia/Shanghai --name {0} -d catatnight/{0}" \
 		.format(app_name, args.radius_secret, args.mysql_server, args.mysql_login, args.mysql_passwd, "yes" if args.readsqlclients else "no"), \
+	"debug": \
+		"docker run --net=host -e radpass={1} -e mysql_server={2} -e mysql_login={3} -e mysql_passwd={4} " \
+		"-e readsqlclients={5} -e time_zone=Asia/Shanghai --name {0} -it catatnight/{0} /bin/bash" \
+		.format(app_name, args.radius_secret, args.mysql_server, args.mysql_login, args.mysql_passwd, "yes" if args.readsqlclients else "no"), \
 	"start": "docker start %s" % app_name, \
 	"stop": "docker stop %s" % app_name, \
 	"restart": "docker restart %s" % app_name, \
 	"delete" : "docker rm -f %s" % app_name }
 cmd_key = args.execute
+
+if cmd_key == 'debug':
+	process = subprocess.call(shlex.split(cmd_dict[cmd_key]))
+	exit(0)
 
 process = subprocess.Popen(shlex.split(cmd_dict[cmd_key]), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 if process.stdout.readline():
