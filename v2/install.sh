b2c06d3496ef3d795fd55f4030966406817ce884
Minghou Ye <minghou.ye@gmail.com>
2014-09-11 14:09:34 +0800

    cleaner code

--- v2/install.sh
+++ v2/install.sh
@@ -2,39 +2,60 @@
 
 #initialization
 if [[ "$(cat /etc/freeradius/dictionary | grep 'Max-Monthly-Traffic')" != "" ]]; then
-  exit 0
+	exit 0
 fi
 
 #freeradius
-if [[ $readsqlclients == "no" ]]; then
-  sed -i -e "/client localhost/i client 0.0.0.0/0{\n\tsecret = $radpass\n}" \
-    -e "/client localhost/i client ipv6{\n\tipv6addr = ::\n\tsecret = $radpass\n}" \
-    -e "s/testing123/$radpass/" /etc/freeradius/clients.conf
-else
-  sed -i "/readclients/ s/#//" /etc/freeradius/sql.conf
-  sed -i "/clients.conf$/ s/^/#/" /etc/freeradius/radiusd.conf
+#support ipv6
+cat >> /etc/freeradius/radiusd.conf <<EOF
+listen {
+	ipv6addr = ::
+	port = 0
+	type = auth
+}
+listen {
+	ipv6addr = ::
+	port = 0
+	type = acct
+}
+EOF
+#def clients
+cat >> /etc/freeradius/clients.conf <<EOF
+client 0.0.0.0/0 {
+	secret = $radpass
+}
+client ipv6 {
+	ipv6addr = ::
+	secret = $radpass
+}
+EOF
+sed -i "s/testing123/$radpass/" /etc/freeradius/clients.conf
+if [[ $readsqlclients == "yes" ]]; then
+	sed -i "/readclients/ s/#//" /etc/freeradius/sql.conf
+	sed -i "/clients.conf$/ s/^/#/" /etc/freeradius/radiusd.conf
 fi
-sed -i -e '/sql.conf$/ s/#//' \
-  -e "1i listen {\n\tipv6addr = ::\n\tport = 0\n\ttype = auth\n}" \
-  -e "1i listen {\n\tipv6addr = ::\n\tport = 0\n\ttype = acct\n}" /etc/freeradius/radiusd.conf
-sed -i -e "s/server = \"localhost\"/server = \"$mysql_server\"/" \
-  -e "s/login = \"radius\"/login = \"$mysql_login\"/" \
-  -e "s/password = \"radpass\"/password = \"$mysql_passwd\"/" /etc/freeradius/sql.conf
-sed -i "/simul_count_query =/,+3 s/#//" /etc/freeradius/sql/mysql/dialup.conf
+#add sql module
+sed -i '/sql.conf$/ s/#//' /etc/freeradius/radiusd.conf
+sed -i -e "/^\tserver/ s/localhost/$mysql_server/" \
+	-e "/^\tlogin/ s/radius/$mysql_login/" \
+	-e "/^\tpassword/ s/radpass/$mysql_passwd/" /etc/freeradius/sql.conf
 sed -i 's/^#[ \t]sql$/\tsql/' /etc/freeradius/sites-available/default
 sed -i 's/^#[ \t]sql$/\tsql/' /etc/freeradius/sites-available/inner-tunnel
+#default_eap_type
 sed -i '0,/md5/{s/md5/mschapv2/}' /etc/freeradius/eap.conf
-#max monthly traffic
+#Simultaneous-Use
+sed -i "/simul_count_query =/,+3 s/#//" /etc/freeradius/sql/mysql/dialup.conf
+#Max-Monthly-Traffic
 sed -i '/counter.conf/ s/#//' /etc/freeradius/radiusd.conf
 cat >> /etc/freeradius/sql/mysql/counter.conf <<EOF
 sqlcounter monthlytrafficcounter {
-    counter-name = Monthly-Traffic
-    check-name = Max-Monthly-Traffic
-    reply-name = Monthly-Traffic-Limit
-    sqlmod-inst = sql
-    key = User-Name
-    reset = monthly
-    query = "SELECT SUM(acctinputoctets + acctoutputoctets) FROM radacct WHERE UserName='%{%k}' AND UNIX_TIMESTAMP(AcctStartTime) > '%b'"
+	counter-name = Monthly-Traffic
+	check-name = Max-Monthly-Traffic
+	reply-name = Monthly-Traffic-Limit
+	sqlmod-inst = sql
+	key = User-Name
+	reset = monthly
+	query = "SELECT SUM(acctinputoctets + acctoutputoctets) FROM radacct WHERE UserName='%{%k}' AND UNIX_TIMESTAMP(AcctStartTime) > '%b'"
 }
 EOF
 sed -i '/^\tpap/a\\tmonthlytrafficcounter' /etc/freeradius/sites-available/default
